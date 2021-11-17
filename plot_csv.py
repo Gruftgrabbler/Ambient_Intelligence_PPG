2f59d544b3556edd3254fecb62b75a77890aee48
pwtll <82121258+pwtll@users.noreply.github.com>
2021-11-17 16:24:31 +0100

    - csv logging
    - csv plot

---
+++ plot_csv.py
@@ -0,0 +1,46 @@
+import matplotlib as mpl
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cbook as cbook
+import glob
+import os
+
+params = {'mathtext.default': 'regular',
+          'axes.titlesize': 16,
+          'axes.labelsize': 14,
+          'font.family' : 'sans-serif',
+          'font.sans-serif' : 'Tahoma'
+          }        # mathematische Achsenbeschriftungen
+plt.rcParams.update(params)
+
+list_of_files = glob.glob('C:/Users/Philipp Witulla/PycharmProjects/AmbientIntellicence/Sensordaten/*.csv') # * means all if need specific format then *.csv
+latest_file = max(list_of_files, key=os.path.getctime)
+print(latest_file)
+
+def read_datafile(file_name):
+    # the skiprows keyword is for skipping the csv header
+    data = np.loadtxt(file_name, delimiter=',', skiprows=1)
+    return data
+
+data = read_datafile(latest_file)
+
+time = data[:,0]
+sensor1 = data[:,1]
+sensor2 = data[:,2]
+
+fig = plt.figure()
+
+ax1 = fig.add_subplot(111)
+
+ax1.set_title("MAX30102 Serial Readings")
+ax1.set_xlabel('Time $(ms)$')
+ax1.set_ylabel('Amplitude')
+
+ax1.plot(time,sensor1, c='r', label='Red')
+ax1.plot(time,sensor2, c='b', label='IR')
+
+leg = ax1.legend()
+plt.grid()
+plt.tight_layout()
+
+plt.show()
---
+++ save_serial_to_csv.py
@@ -0,0 +1,60 @@
+import os
+import csv
+import time
+import warnings
+#import serial
+import serial.tools.list_ports
+
+file_path = "Sensordaten/"
+file_name = time.strftime("%Y%m%d-%H%M%S") + '.csv'
+
+arduino_ports = [
+    p.device
+    for p in serial.tools.list_ports.comports()
+    #if 'Arduino' in p.description
+    #if 'USB-SERIAL' in p.description
+    if 'Silicon Labs CP210x' in p.description
+]
+if not arduino_ports:
+    raise IOError("No Arduino found")
+if len(arduino_ports) > 1:
+    warnings.warn('Multiple Arduinos found - using the first')
+
+Arduino = serial.Serial(arduino_ports[0], 9600)         # initialisiert ersten gefundenen Arduino mit Baud-Rate 9600
+Arduino.flush()
+Arduino.reset_input_buffer()
+
+if not os.path.exists(file_path):
+    os.makedirs(file_path)
+
+with open(file_path + file_name, 'w', newline='') as outfile:
+    fieldnames = ['time', 'sensor1', 'sensor2']
+
+    csv_writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+    csv_writer.writeheader()
+
+# start_time = int(round(time.time() * 1000))         # start_time = time.time()
+
+while True:
+    while (Arduino.inWaiting() == 0):
+        pass
+    try:
+
+        data = Arduino.readline()
+        dataarray = data.decode().rstrip().split(',')
+        #Arduino.reset_input_buffer()
+
+        # current_time = int(round(time.time() * 1000)) - start_time  # time.time() - start_time
+        millis = float(dataarray[0])
+        sensor_red = float(dataarray[1])
+        sensor_ir = float(dataarray[2])
+
+        print(millis, ",", sensor_red, ",", sensor_ir)
+
+        with open(file_path + file_name, 'a', newline='') as outfile:
+            csv_writer = csv.writer(outfile)
+            csv_writer.writerow([millis, sensor_red, sensor_ir])
+    except(KeyboardInterrupt, SystemExit,IndexError,ValueError):
+        pass
+
+    outfile.close()
