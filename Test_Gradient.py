7ecf6bfe38bb42139bf4007e7b3f377f25ebfbde
Gruftgrabbler <gruftgrabbler@googlemail.com>
2022-01-17 15:39:33 +0100

    Test Gradient

--- Test_Gradient.py
+++ Test_Gradient.py
@@ -1,6 +1,8 @@
 import numpy as np
-from matplotlib import pyplot as plt
+import scipy
+from scipy.fft import fft, fftfreq
 from scipy.signal import butter, lfilter, freqz
+from matplotlib import pyplot as plt
 
 
 # TODO Print FFT of singal
@@ -16,6 +18,7 @@ def butter_lowpass(cutoff_freq, fs, order=4):
     nyq = 0.5 * fs
     normal_cutoff = cutoff_freq / nyq
     b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    print(nyq, cutoff_freq, normal_cutoff)
     return b, a
 
 
@@ -37,15 +40,18 @@ def butter_lowpass_filter(data, cutoff_freq, fs, order=4):
 sample_rate = 44100  # [Hz ]
 freq_fund = 100  # fundamental freq [Hz]
 num_periods = 10  # number of fundamental sinewave periods
-num_samples = sample_rate * num_periods  # total number of samples
+num_samples = int(sample_rate * num_periods)  # total number of samples
 
-x = np.linspace(0, num_periods / freq_fund, num_samples)
+x = np.linspace(0, num_periods / freq_fund, num_samples, endpoint=False)
+# NOTE: Spacing between samples is (stop - start) / (num_samples - 1)
+#       So: (num_periods / freq_fund - 0) / (num_samples - 1)
+sample_spacing = (num_samples / freq_fund) / (num_samples - 1)
 
 y_fund = np.sin(freq_fund * 2 * np.pi * x)
-y_overlapped = y_fund + 0.2 * np.sin(5 * freq_fund * 2 * np.pi * x) + 0.1 * np.sin(10 * freq_fund * 2 * np.pi * x)
+y_overlapped = y_fund + 0.2 * np.sin(10 * freq_fund * 2 * np.pi * x) + 0.1 * np.sin(20 * freq_fund * 2 * np.pi * x)
 
 # Filter the high frequency content with a 4pole lowpass
-cutoff = 150  # [Hz]
+cutoff = 100  # [Hz]
 order = 6
 
 # get the filter coefficients
@@ -54,9 +60,13 @@ b, a = butter_lowpass(cutoff, sample_rate, order)
 # filter the signal
 y_filtered = butter_lowpass_filter(y_overlapped, cutoff, sample_rate, order)
 
+# create FFTs
+y_overlapped_fft = fft(y_overlapped)
+# xf_overlapped_fft = np.linspace(0.0, 1.0/(2.0*sample_spacing), int(num_samples / 2))
+xf = fftfreq(num_samples, sample_spacing)[:num_samples // 2]
 # Plot frequency respone of filter
 plot_rows = 2
-plot_cols = 2
+plot_cols = 3
 
 W, h = freqz(b, a)  # get the filter response
 plt.subplot(plot_rows, plot_cols, 1)
@@ -85,5 +95,12 @@ plt.subplot(plot_rows, plot_cols, 4)
 plt.plot(x, y_filtered, label='filtered signal')
 plt.grid()
 plt.legend()
+
+plt.subplot(plot_rows, plot_cols, 5)
+plt.plot(xf, 2.0 / num_samples * np.abs(y_overlapped_fft[:num_samples // 2]))
+plt.grid()
+
+plt.subplot(plot_rows, plot_cols, 6)
+plt.grid()
 
 plt.show()
